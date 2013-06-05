6706f86fa5b03904f68a5c51ab33f0a1bfe5cf1d
Antti Kaihola <antti@2general.com>
2013-06-05 12:32:34 +0300

    Added more metadata to setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,19 @@ from setuptools import setup
 
 setup(name='django-grains',
       version='0.1',
-      packages=['grains'])
+      description=('Define content fragments in Django templates '
+                   'and store their contents in the database. '
+                   'This is yet another take '
+                   'on the idea previously implemented in '
+                   'django-flatblocks, django-chunks etc.'),
+      author='Antti Kaihola (2General Ltd.)',
+      author_email='antti@2general.com',
+      url='http://github.com/2general/django-grains',
+      packages=['grains'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Internet :: WWW/HTTP',
+                   'License :: OSI Approved :: BSD License'])
